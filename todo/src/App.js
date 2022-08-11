80b96ff90d1ff5ad7f341d13f35fc3bd3908b5a7
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2022-08-11 12:06:30 +0530

    todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const addTask = () => {
+    setTodo([...todo, currentTask]);
+  };
   return (
     <div className='App'>
       <h1>ToDo List</h1>
+      <div>
+        <input
+          type='text'
+          placeholder='Task'
+          onChange={(e) => {
+            setCurrentTask(e.target.value);
+          }}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <hr />
+      <ul>
+        {todo.map((val, key) => {
+          return <li key={key}>{val}</li>;
+        })}
+      </ul>
     </div>
   );
 }
